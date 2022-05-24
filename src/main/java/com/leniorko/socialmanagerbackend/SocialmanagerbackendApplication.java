0a36ff45476bbda6b504707a6ce3c735237b8d11
Ivan Egorov <www.Len244@gmail.com>
2022-05-24 23:08:18 +0300

    Added test route. Trying to push with EGit from Eclipse

--- src/main/java/com/leniorko/socialmanagerbackend/SocialmanagerbackendApplication.java
+++ src/main/java/com/leniorko/socialmanagerbackend/SocialmanagerbackendApplication.java
@@ -2,12 +2,20 @@ package com.leniorko.socialmanagerbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SocialmanagerbackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialmanagerbackendApplication.class, args);
+	}
+	
+	@GetMapping("/test")
+	String testing() {
+		return "THIS WORKING";
 	}
 
 }
